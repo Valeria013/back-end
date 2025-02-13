1843df7d8995e8de0f647ce5bc874566c3377037
valeria013 <valeriagoncalves0401@gmail.com>
2025-02-13 07:50:48 -0300

    ...

--- router/users.js
+++ router/users.js
@@ -1,17 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 import { Router } from 'express';
-const saltRounds = 10;
 
 const userRouter = Router()
 const prisma = new PrismaClient()
 
 userRouter.get('/users', async (req, res) => {
-  try {
-    const users = await prisma.user.findMany({});
-    res.json(users)
-  } catch (err) {
-    res.status(400).send('Erro ao buscar usuários')
-  }
+  const users = await prisma.user.findMany({});
+  res.json(users)
 })
 
 export default userRouter
